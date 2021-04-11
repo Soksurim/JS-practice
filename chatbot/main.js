6668c2f4cb6e12d9df264caaba7b744f90d3a43f
Soksurim <briochefavori@gmail.com>
2021-04-11 13:44:13 +0900

    updated chatbot

--- chatbot/main.js
+++ chatbot/main.js
@@ -1,54 +1,90 @@
-let msg = ":회원가입";
-let isGroupChat = false;
-let ImageDB = "123123"; 
+const scriptName = "json";
+let timerSwitch = false; // timer on/off
+/**
+ * (string) room
+ * (string) sender
+ * (boolean) isGroupChat
+ * (void) replier.reply(message)
+ * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환
+ * (string) imageDB.getProfileBase64()
+ * (string) packageName
+ */
 
-let timerSwitch = false;
+function response(
+  room,
+  msg,
+  sender,
+  isGroupChat,
+  replier,
+  imageDB,
+  packageName
+) {
 
-function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) { 
-
-let worked = false;
-
-if(msg.match(":회원가입") && !isGroupChat){
-    
+  // bot status
+  if (msg.match("/ㅂ")) { bot_status(replier); } 
+  else if (msg.startsWith("/타이머")) { timer(msg, replier); } 
+  else if (msg.match("/회원가입")){ register(replier, sender, imageDB, isGroupChat) }
+  else { replier.reply(sender + "님이 " + msg + "라고 보내셨습니다."); }
 }
 
-if(msg.includes("ㅂ") && !worked){
 
+//아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다.
+function onCreate(savedInstanceState, activity) {
+  var textView = new android.widget.TextView(activity);
+  textView.setText("Hello, World!");
+  textView.setTextColor(android.graphics.Color.DKGRAY);
+  activity.setContentView(textView);
+}
+function onStart(activity) {}
+function onResume(activity) {}
+function onPause(activity) {}
+function onStop(activity) {}
+
+
+/* ########################
+  메소드
+#########################*/
+
+function bot_status(replier) {
   var bt = Device.getBatteryLevel();
-   var ch = Device.isCharging();
-
-replier.reply("현재 봇 상태 \n 배터리 : " + bt + "%\n 충전중 : " + ch);
-worked = true;
+  var ch = Device.isCharging();
+  replier.reply(
+    "현재 봇 상태 \n 배터리 : " + bt + "%\n 충전중 : " + ch + "\n" + new Date()
+  );
+  replier.reply(Utils.getPhoneBrand() + "\n" + Utils.getPhoneModel());
+  DataBase.appendDataBase(
+    "TimeLog.json",
+    "현재 봇 상태 \n 배터리 : " + bt + "%\n 충전중 : " + ch + "\n" + new Date() + "\n" );
+  DataBase.getDataBase("TimeLog.json");
+  worked = true;
 }
 
-if (msg.startsWith("타이머") && !timerSwitch && !worked) {
-  msg = msg.replace("타이머", "").trim();
-worked = true;
-  if (msg.match(/^[0-9]*$/)) {
-    timer(msg);
-    
+function timer(msg, replier) {
+  msg = msg.replace("/타이머", "").trim();
+  if (timerSwitch) {
+    replier.reply("타이머가 이미 동작중입니다.");
+  } else if (!msg.match(/^[0-9]*$/) || msg.length == 0) {
+    replier.reply("/타이머 (초) 형식으로 입력해주세요");
   } else {
-    replier.reply("타이머(숫자) 형식으로 다시 보내주세요");
+    timeCount(msg, replier);
   }
-} else if(msg.startsWith("타이머") && timerSwitch) {
-  replier.reply("타이머가 이미 동작중입니다.");
-worked = true;
 }
 
- function timer(sec) {
+function timeCount(sec, replier) {
   timerSwitch = true;
-
   while (sec > 0) {
     replier.reply("남은 시간 : " + sec);
-    Utils.sleep(1000);
+    java.lang.Thread.sleep(1000);
     sec--;
   }
-
   timerSwitch = false;
   replier.reply("타이머 끝");
 }
-if(!worked){
-replier.reply(sender+ "님이 "+msg+ "라고 보내셨습니다.");  
-}
-w
+
+function register(replier, sender, imageDB, isGroupChat) { // 이미 가입된 계정도 
+
+  if(isGroupChat) replier.reply("회원가입은 1:1채팅에서 진행해주세요!\nhttps://open.kakao.com/o/sDeNqs7c");
+  else{
+    replier.reply("계정명 : " + sender + "\n프로필 암호 : " + imageDB.getProfileBase64() + "\n로 프로필을 생성합니다.");
+  }
 }
--- chatbot/prac.js
+++ chatbot/prac.js
@@ -1,104 +1,93 @@
-let room = "roomName";
-let msg = "응애";
-let sender = "sender";
-let isGroupChat = false;
-let replier;
-let ImageDB;
-let packageName = "kakaotalk";
+// main 백업
 
-//경로 : /storage/emulated/0/ChatBot/BotData/game/userData/
+
+const scriptName = "json";
+let timerSwitch = false; // timer on/off
+/**
+ * (string) room
+ * (string) sender
+ * (boolean) isGroupChat
+ * (void) replier.reply(message)
+ * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환
+ * (string) imageDB.getProfileBase64()
+ * (string) packageName
+ */
+
 function response(
   room,
   msg,
   sender,
   isGroupChat,
   replier,
-  ImageDB,
+  imageDB,
   packageName
 ) {
-  /* 코드 시작 */
 
-  if (msg.includes("ㅇ")) {
-    let textData = {
-      room: room,
-      sender: sender,
-      msg: msgs,
-    };
+  // bot status
+  if (msg.match("/ㅂ")) { bot_status(replier); } 
+  else if (msg.startsWith("/타이머")) { timer(msg, replier); } 
+  else if (msg.match("/회원가입")){ register(replier, sender, imageDB, isGroupChat) }
+  else { replier.reply(sender + "님이 " + msg + "라고 보내셨습니다."); }
+}
+
+
+//아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다.
+function onCreate(savedInstanceState, activity) {
+  var textView = new android.widget.TextView(activity);
+  textView.setText("Hello, World!");
+  textView.setTextColor(android.graphics.Color.DKGRAY);
+  activity.setContentView(textView);
+}
+function onStart(activity) {}
+function onResume(activity) {}
+function onPause(activity) {}
+function onStop(activity) {}
+
+
+/* ########################
+  메소드
+#########################*/
+
+function bot_status(replier) {
+  var bt = Device.getBatteryLevel();
+  var ch = Device.isCharging();
+  replier.reply(
+    "현재 봇 상태 \n 배터리 : " + bt + "%\n 충전중 : " + ch + "\n" + new Date()
+  );
+  replier.reply(Utils.getPhoneBrand() + "\n" + Utils.getPhoneModel());
+  DataBase.appendDataBase(
+    "TimeLog.json",
+    "현재 봇 상태 \n 배터리 : " + bt + "%\n 충전중 : " + ch + "\n" + new Date() + "\n" );
+  DataBase.getDataBase("TimeLog.json");
+  worked = true;
+}
+
+function timer(msg, replier) {
+  msg = msg.replace("/타이머", "").trim();
+  if (timerSwitch) {
+    replier.reply("타이머가 이미 동작중입니다.");
+  } else if (!msg.match(/^[0-9]*$/) || msg.length == 0) {
+    replier.reply("/타이머 (초) 형식으로 입력해주세요");
+  } else {
+    timeCount(msg, replier);
   }
-
-  JSON.parse(readJson);
-
-  /* 코드 끝 */
 }
 
-function timenow() {
-  var day = new Date();
-  day.getFullYear +
-    "/" +
-    (day.getMonth() + 1) +
-    "/" +
-    day.getDate() +
-    " " +
-    day.getHours +
-    ":" +
-    day.getMinutes() +
-    ":" +
-    day.getSeconds() +
-    ":" +
-    day.getMilliseconds();
+function timeCount(sec, replier) {
+  timerSwitch = true;
+  while (sec > 0) {
+    replier.reply("남은 시간 : " + sec);
+    java.lang.Thread.sleep(1000);
+    sec--;
+  }
+  timerSwitch = false;
+  replier.reply("타이머 끝");
 }
 
-// 경로 : /storage/emulated/0/ChatBot/BotData/game/userData/
+function register(replier, sender, imageDB, isGroupChat) { // 이미 가입된 계정도 
 
-let path = "/storage/emulated/0/ChatBot/BotData/game/userData/";
-
-function response(
-  room,
-  msg,
-  sender,
-  isGroupChat,
-  replier,
-  ImageDB,
-  packageName
-) {
-  //let data = room + " // " + sender + " : " + msg + "\n";
-  var day = new Date();
-  var time =
-    day.getFullYear() +
-    "/" +
-    (day.getMonth() + 1) +
-    "/" +
-    day.getDate() +
-    " " +
-    day.getHours() +
-    ":" +
-    day.getMinutes() +
-    ":" +
-    day.getSeconds() +
-    "." +
-    day.getMilliseconds();
-  print(time);
-
-  let textData = {
-    room: room,
-    sender: sender,
-    msg: msg,
-    time: time,
-  };
-
-  var objData = JSON.stringify(textData);
-
-  //print(textData.msg);
-
-  FileStream.createDir(path + "/folder");
-
-  File.save(path + sender + ".json", objData, true); // true면 append, false면 cover
-
-  //var readJson = FileStream.readJson(path + sender + ".json");
-
-  //var parsed = JSON.parse(readJson);
+  if(isGroupChat) replier.reply("회원가입은 1:1채팅에서 진행해주세요!\nhttps://open.kakao.com/o/sDeNqs7c");
+  else{
+    replier.reply("계정명 : " + sender + "\n프로필 암호 : " + imageDB.getProfileBase64() + "\n로 프로필을 생성합니다.");
+  }
 }
-
-
-
-
