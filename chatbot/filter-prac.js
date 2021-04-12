d36d4a025df7da774b20a448c8ac35f770879e21
Soksurim <briochefavori@gmail.com>
2021-04-13 01:03:38 +0900

    fixed chatbot error

--- chatbot/filter-prac.js
+++ chatbot/filter-prac.js
@@ -1,7 +1,21 @@
-var json = [{ "key1" : "val1", "key2" : "val2"},{ "key1" : "2val1", "key2" : "2val2"},{ "key1" : "3val1", "key2" : "3val2"}];
+var json = [{ "key1" : "val1", "key2" : "val2"},{ "key1" : "surimi", "key2" : "2val2"},{ "key1" : "3val1", "key2" : "3val2"}];
 
 //console.log(json);
 //console.log(json[0]);
 //console.log(JSON.stringify(json));
 
-console.log(json.filter(e => e.key1 == "val1"));
+var sender = "surimi"
+
+//console.log(json.filter(e => e.key1 == sender)[0].key2);
+
+json.filter(e => e.key1 == sender)[0].key2 = "AngAng";
+
+let j2 = json.filter(e => e.key1 == sender)[0].key2;
+
+j2 = "Mario";
+
+console.log(json);
+console.log(j2);
+
+//한번에 여러 값을 수정할때엔 인덱스 값을 구하는게 나음.
+// 배열 하나를 출력하거나 값 하나를 수정할땐 filter가 편할듯.
--- chatbot/main.js
+++ chatbot/main.js
@@ -1,8 +1,13 @@
 // 가입시 닉네임 제한 숫자닉 등 불가
-// filter 써보기 외부변수 -> 내부함수
 // 내 정보 출력시키기
 // 가입시 기본 유저데이터 생성
 // 유저 순위 표시
+
+
+
+// [완료]
+// filter 써보기 외부변수 -> 내부함수
+// 회원 가입 안하면 명령어 사용 x
 
 /***********************************************
                수정 전 백업할 것!!!!
@@ -13,7 +18,7 @@ const scriptName = "json";
 let timerSwitch = false; // timer on/off
 /**
  * (string) room
- * (string) sender
+ * (string) sende
  * (boolean) isGroupChat
  * (void) replier.reply(message)
  * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환
@@ -35,18 +40,21 @@ function response(
   packageName
 ) {
 
-  if(room != "surimi_bot"){
-    return;
-  }
-  
-  update(sender, replier);
-
-  // bot status
-  if (msg.match("/ㅂ")) { bot_status(replier); } 
-  else if (msg.startsWith("/타이머")) { timer(msg, replier); } 
+  if(room != "surimi_bot"){ return; }
   else if (msg.match("/ㅎ")){ register(replier, sender, imageDB, isGroupChat); }
-  else if (msg.match("/ㅇㅈ")){ showUserList(replier); }
-  else { replier.reply(sender + "님이 " + msg + "라고 보내셨습니다."); }
+  else if(msg.startsWith("/")){
+    let profileList = JSON.parse(DataBase.getDataBase("profileList.txt"));
+    var data = profileList.filter(e => e.name == sender);
+    if(data.length == 0){ replier.reply("회원가입을 먼저 해주세요\n명령어 : /ㅎ \n회원가입은 1:1채팅에서만 가능합니다.\nhttps://open.kakao.com/o/sDeNqs7c"); return; }
+
+    update(sender, profileList);
+    if (msg.match("/ㅂ")) { bot_status(replier); } 
+    else if (msg.startsWith("/타이머")) { timer(msg, replier); } 
+    //else if (msg.match("/ㅎ")){ register(replier, sender, imageDB, isGroupChat, profileList); }
+    else if (msg.match("/ㅇㅈ")){ showUserList(replier, profileList); }
+    else { replier.reply(sender + "님이 " + msg + "라고 보내셨습니다."); }
+  }
+
 }
 
 
@@ -102,55 +110,36 @@ function timeCount(sec, replier) {
   replier.reply("타이머 끝");
 }
 
-function register(replier, sender, imageDB, isGroupChat) { // 이미 가입된 계정도 
+function register(replier, sender, imageDB, isGroupChat, profileList) { // 이미 가입된 계정도 
   
   if(isGroupChat) replier.reply("회원가입은 1:1채팅에서 진행해주세요!\nhttps://open.kakao.com/o/sDeNqs7c");
   else{  
+    let profileList = JSON.parse(DataBase.getDataBase("profileList.txt"));
+    var data = profileList.filter(e => e.name == sender);
     
-    var profileList = JSON.parse(DataBase.getDataBase("profileList.txt"));
-    // 실험 필요 profileList.filter( function(e, sender){ return e.name == sender; })
-    
-    let indexNum = -1;
-    
-    for(index in profileList){
-      if(profileList[index].name == sender){
-        indexNum = index;
-      }
-    }
-    
-    if(indexNum != -1){
-      replier.reply("이미 존재하는 계정입니다.\n가입일 : " + profileList[indexNum].registerDate);
+    if(data.length > 0){
+      replier.reply("이미 존재하는 계정입니다.\n가입일 : " + data[0].registerDate);
     }
     else{
       replier.reply("계정명 : " + sender + "\n프로필 고유번호 : " + imageDB.getProfileBase64().substr(100,120)+ "\n로 프로필을 생성합니다.");
       var parsed = JSON.parse(DataBase.setDataBase(sender + "_profile.txt", JSON.stringify({"name" : sender, "idkey" : imageDB.getProfileBase64()})));
       replier.reply(parsed.name + "님 환영합니다!" );
    
-      var profileList = JSON.parse(DataBase.getDataBase("profileList.txt"));
       profileList.push({"id" : profileList.length, "name" : sender, "registerDate" : getDate(), "lastActive" : getDate()});
       DataBase.setDataBase("profileList.txt", JSON.stringify(profileList));
-
    }
   }
   
 }
-function showUserList(replier) {
+function showUserList(replier, profileList) {
   var txt = "[유저목록]\n";
-  var db = JSON.parse(DataBase.getDataBase("profileList.txt"));
-  for(i in db ){ txt += i + ". " + db[i].name + " " + db[i].registerDate + "\n"; }
+  for(i in profileList ){ txt += i + ". " + profileList[i].name + " " + profileList[i].registerDate + "\n"; }
   replier.reply(txt);
 }
 
-function update(sender, replier){
-  var db = JSON.parse(DataBase.getDataBase("profileList.txt"));
-  
-  for(index in db){
-    if(db[index].name.mathch(sender)){
-      db[index].name = "surimi";
-      db[index].lastActive = getDate();
-    }
-  }
-  DataBase.setDataBase("profileList.txt", JSON.stringify(db));
+function update(sender, profileList){
+  profileList.filter(e => e.name == sender)[0].lastActive = getDate();
+  DataBase.setDataBase("profileList.txt", JSON.stringify(profileList));
 }
 
 function getDate(){
